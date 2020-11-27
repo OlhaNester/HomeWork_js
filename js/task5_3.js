94912d24cd4ff822a7e322feccd4e5b6c3198516
OlhaNester <olga_nv@ukr.net>
2020-11-27 14:56:53 +0200

    5-й модуль

---
+++ js/task5_3.js
@@ -0,0 +1,52 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+    return this.items;
+  }
+
+  removeItem(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  }
+}
+
+console.log(typeof Storage);
+// 'function'
+
+const goods = ['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор'];
+
+const storage = new Storage(goods);
+
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+storage.addItem('Дроид');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+storage.removeItem('Пролонгер');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
