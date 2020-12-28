ca6ba11bb33ac418c8879ecabcc9d6b155c28cdc
OlhaNester <olga_nv@ukr.net>
2020-12-28 22:05:01 +0200

    задача 34 модуль из 41

--- js/task5_3.js
+++ js/task5_3.js
@@ -1,18 +1,25 @@
+//Напиши класс Storage, который будет создавать объекты для управления складом товаров.
+//При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+//Добавь методы класса:
+
+//getItems() - возвращает массив текущих товаров
+//addItem(item) - получает новый товар и добавляет его к текущим
+//removeItem(item) - получает товар и, если он есть, удаляет его из текущих
+
+// Write code under this line
 class Storage {
   constructor(items) {
     this.items = items;
   }
-
   getItems() {
-    return this.items;
+    return goods;
   }
 
   addItem(item) {
     this.items.push(item);
-    return this.items;
   }
-
-  removeItem(itemName) {
+  removeItem(item) {
     this.items = this.items.filter(item => item !== itemName);
   }
 }
